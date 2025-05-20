6d80c4b915efb3542521169d30f9e695846ffdcc
madisonh <alsuabsattarova648@gmail.com>
2025-05-21 01:04:42 +0300

    +models

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - `pyhon -m venv venv` - создание виртуального окружения
 - `sourse venv/bin/activate` - войти в виртуальное окружение
 - `pip install -r requirements` - установка зависимостей
+- `python manage.py runserver` - запустить сервер для разработки на http://127.0.0.1:8000
--- web/models.py
+++ web/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=50)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()  # Подробное описание продукта.
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=255)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # Дата и время создания заказа
+    status = models.CharField(max_length=50)
+
+class OrderItem(models.Model):  # Позиция заказа
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()  # Количество единиц продукта в заказе
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()  # Оценка
+    comment = models.TextField()
+
+class Promotion(models.Model):  # Акция
+    name = models.CharField(max_length=100)
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Supplier(models.Model):  # Поставщик
+    name = models.CharField(max_length=100)
+    contact_info = models.TextField()
+    
