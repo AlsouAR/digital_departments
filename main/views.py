57ca83a849652db11e4b521cd041e4901243f708
madisonh <alsuabsattarova648@gmail.com>
2025-05-24 01:14:14 +0300

    Сделана маршрутизация для index и about

--- main/views.py
+++ main/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 def index(request):
     context: dict[str, str] = {
         'title': 'Home',
-        'content': 'Главная страница кондитерской - Крендель'
+        'content': 'Sweet Point - Главная'
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict[str, str] = {
+        'title': 'О нас',
+        'content': 'Sweet Point - О нас'
+    }
+
+    return render(request, 'main/about.html', context)
